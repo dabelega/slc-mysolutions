3ec053dcd0c64ff3da2ef0844408945317c1bb75
DHANI ABELEGA <shani@DHANI-ABELEGAs-MacBook-Pro.local>
2017-04-08 08:39:46 +0100

    app files added

---
+++ app/library.js
@@ -0,0 +1,17 @@
+function fizzBuzz(num){
+  var msg = '';
+  if (num % 3!== 0 && num % 5 !== 0){
+    return num;
+  }
+  
+  else {
+    if (num % 3 === 0){
+      msg += "Fizz";
+   }
+    if (num % 5 === 0){
+      msg += "Buzz";
+    }
+  }
+  return msg;
+}
+
